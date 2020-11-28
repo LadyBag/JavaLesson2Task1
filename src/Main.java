7c96705fa5fd0962ff14955293ff6415c378fa62
Levina Ekaterina <ashokoladova@yandex.ru>
2020-11-28 21:58:25 +0300

    урок2

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main (String[] params)
+    {
+        int balance = 2_000_000_000;
+        int summ = 500_000_000;
+        int newbalance = balance + summ;
+
+
+    }
+}
